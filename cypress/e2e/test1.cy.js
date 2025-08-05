b3fbad6d403436f66cccc05d9c2807486b7332dc
Your Name <you@example.com>
2025-08-06 00:05:19 +0530

    Checkbx, Dropdown and Invisible Element Handling

--- cypress/e2e/test1.cy.js
+++ cypress/e2e/test1.cy.js
@@ -7,7 +7,7 @@ describe('GreenKart Test Suite', function(){  it('Should be able to Load the Hom
     // cy.get('.product:visible').should('have.length', 4)
 
     // Parent-child chaining
-    cy.get(`.products`).as(`productlocator`)
+    cy.get(`.products`).as(`productlocator`)    //alias method
     cy.get('@productlocator').find('.product').should('have.length', 4)
 
     cy.get('@productlocator').find('.product').eq(2).contains('ADD TO CART').click()  // add third element only. 
@@ -16,14 +16,18 @@ describe('GreenKart Test Suite', function(){  it('Should be able to Load the Hom
       const textVeg = $el.find('.product-name').text()
       if(textVeg.includes('Cashews'))   // to click on a specific product after a loop, used in cases where sequence is dynamic. 
         {
-          cy.wrap($el).contains('ADD TO CART').click()  // click with find() is deprecated. so use wrap
+          cy.wrap($el).contains('ADD TO CART').click()   // click with find() is deprecated. so use wrap 
         }
     })
 
+    // assert if logo is correctly displayed
+    cy.get(`.brand.greenLogo`).should('have.text', 'GREENKART')
+
+    // to print validation in logs
     cy.get(`.brand.greenLogo`).then((LogoElement) => {
       cy.log(LogoElement.text())
     })
-
+    
     
   })
 })
